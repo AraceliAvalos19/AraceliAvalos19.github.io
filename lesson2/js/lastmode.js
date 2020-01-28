418f741ba762a00fa3306f59837d6f12a977505e
AraceliAvalos19 <ava19001@byui.edu>
2020-01-28 11:51:50 -0600

    update

--- lesson2/js/lastmode.js
+++ lesson2/js/lastmode.js
@@ -1,2 +1,31 @@
-const options = {weekday: "long", day: "numeric", month: "long", year: "numeric"}; 
-                document.getElementById("currentdate").textContent = new Date().toLocaleDateString("en-US",options);
+const daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const todaysdate = new Date();
+const dayName = dayNames[todaysdate.getDay()]
+const monthName = months[todaysdate.getMonth()]
+const currentdate = dayName + "," + todaysdate.getDay() + " " + monthName + " , " + todaysdate.getFullYear();
+
+document.getElementById("lastmode").textContent = new Date()
+<script src = "js / date.js"> </script>
