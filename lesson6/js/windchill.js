a1debbbd8dc8e97583993c4fa199f274930f8a8e
AraceliAvalos19 <ava19001@byui.edu>
2020-02-18 12:49:37 -0600

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,16 +1,20 @@
-const temperature = parseFloat(document.getElementById('temp').textContent);
-const windSpeed = parseFloat(document.getElementById('windspeed').textContent);
-const windChill = (35.74 + (0.6215 * temperature) - (35.75 *(Math.pow(windSpeed, 0.16))) + (0.4275 * temperature * (Math.pow(windSpeed, 0.16))));
-document.getElementById('windChill').textContent = Math.ceil(windChill);
+'use strict';
 
-const temperature = parseFloat(document.getElementById('t').textContent);
-const Wind = parseFloat(document.getElementById('s').textContent);
+/**
+ * Calculate the wind chill factor
+ * @see https://www.weather.gov/media/epz/wxcalc/windChill.pdf
+ *
+ * @param  {Float} temperature - temperature in Fahrenheit
+ * @param  {Float} windSpeed - wind speed in miles per hour
+ * @return {Float}
+ */
+function calculateWindChillFactor(temperature, windSpeed) {
+  return 35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  windSpeed ^ 0.16;
+}
 
-if(temperature<=50 && Wind >=3){
-const windC  =(35.74 + (0.6215*temperature)-(35.75*Math.pow(Wind,0.16))+(0.4275*temperature*Math.pow(Wind,0.16))).toFixed(2);
-document.getElementById('windC').textContent=windC;
-}
-else{
-    const windC =('N/A');
-    document.getElementById('windC').textContent=windC;
-}
+const temp = 32,
+ 	 	  windSpeed = 5;
+
+console.log(
+	`The chill factor for temperature "${temp}" and wind speed "${windSpeed}" is "${calculateWindChillFactor(temp, windSpeed)}".`
+);
