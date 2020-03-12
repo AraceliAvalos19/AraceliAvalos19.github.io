88808b6ac55ab4e798e815cf065d5e99cc299fa9
AraceliAvalos19 <ava19001@byui.edu>
2020-03-11 18:25:35 -0600

    update

--- lesson10/api-test.js/weatherapi.js
+++ lesson10/api-test.js/weatherapi.js
@@ -1,8 +1,14 @@
-http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID={3d6a1aee115e55a320ca45412bdf9764}
-const apiURL = "...";
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=3d6a1aee115e55a320ca45412bdf9764";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    document.getElementById('current-temp').textContent = jsObject.main.temp + " °F";
   });
+
+
+
+
+
+
+ 
