48c3532a9b3d79336629401e73841d537cf968cb
AraceliAvalos19 <ava19001@byui.edu>
2020-03-12 16:18:26 -0600

    update

--- lesson5/js/adv.js
+++ lesson5/js/adv.js
@@ -1,6 +1,6 @@
 var whatdate  = new Date();
 
-if (whatdate.getDay() == 5 ) {
+if (whatdate.getDay() == Friday ) {
   document.querySelector("aside").style.display = "block";
 } else {
   document.querySelector("aside").style.display = "none";
