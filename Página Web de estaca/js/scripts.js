c42eed157f0f127c42565cf871405c0aa11ee896
AraceliAvalos19 <ava19001@byui.edu>
2020-04-22 13:12:11 -0500

    update

---
+++ Página Web de estaca/js/scripts.js
@@ -0,0 +1,29 @@
+WebFont.load({
+    google: {
+    families: ["Oswald", "Roboto Slab", "Montserrat"]
+    }
+    });
+
+
+function toggleMenu() {
+    
+    document.getElementById("primaryNav").classList.toggle("hide");
+
+}
+
+function adjustSeverity(severity) {
+    document.getElementById("severity").innerHTML = severity;
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
